68db02b2bbf3897c7fcd123004ef2ff386f92695
Zakk Hoyt <vaporwarewolf@gmail.com>
2015-05-18 23:33:16 -0700

    first commit

---
+++ VWWMapLegendViewDemo/VWWMapLegendView.h
@@ -0,0 +1,13 @@
+//
+//  VWWMapLegendView.h
+//  VWWMapLegendViewDemo
+//
+//  Created by Zakk Hoyt on 5/18/15.
+//  Copyright (c) 2015 Zakk Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VWWMapLegendView : UIView
+
+@end
---
+++ VWWMapLegendViewDemo/VWWMapLegendView.m
@@ -0,0 +1,21 @@
+//
+//  VWWMapLegendView.m
+//  VWWMapLegendViewDemo
+//
+//  Created by Zakk Hoyt on 5/18/15.
+//  Copyright (c) 2015 Zakk Hoyt. All rights reserved.
+//
+
+#import "VWWMapLegendView.h"
+
+@implementation VWWMapLegendView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
