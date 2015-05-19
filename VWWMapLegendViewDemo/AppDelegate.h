202902fc5819c4cb2d1c52960d6e679d6b16962c
Zakk Hoyt <vaporwarewolf@gmail.com>
2015-05-18 23:02:12 -0700

    Initial Commit

---
+++ VWWMapLegendViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VWWMapLegendViewDemo
+//
+//  Created by Zakk Hoyt on 5/18/15.
+//  Copyright (c) 2015 Zakk Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
